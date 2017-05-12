4e2616309f32718c66899d1d24a8539418e1e951
DESKTOP-UG6IRFO\keiyu <keiya94@gmail.com>
2017-05-12 18:52:41 +0200

    Basic movement and camera

---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed = 10;
+
+    private Rigidbody rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        direction = transform.TransformDirection(direction);
+        rigidBody.velocity = direction * speed;
+    }
+}
